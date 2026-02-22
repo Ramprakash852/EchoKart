37be1afffb6f7b1be8d38a49662e00ee48016655
RAM PRAKASH <chitti852852@gmail.com>
2026-02-22 18:27:35 +0530

    Initial Commit

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
       const[loading,setLoading]=useState(false)  
     let [show,setShow]=useState(false)
             let {serverUrl}= useContext(authDataContext)
-                 let [email,setEmail]=useState("admin@onecart.com")
+                 let [email,setEmail]=useState("admin@echocart.com")
             let [password,setPassword]=useState("admin1234567")
             let {adminData,getAdmin} =useContext(adminDataContext)
 
@@ -42,11 +42,11 @@ function Login() {
         <div className='w-[100vw] h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] text-[white] flex flex-col items-center justifiy-start'>
                  <div className="w-[100%] h-[80px] flex items-center justify-start px-[30px] gap-[10px] cursor-pointer" >
                      <img className="w-[40px]" src={logo} alt="" />
-                     <h1 className="text-[22px] font-sans">One Cart</h1>
+                     <h1 className="text-[22px] font-sans">Echo Cart</h1>
                  </div>
                 <div className="w-[100%] h-[100px] flex items-center justify-center flex-col gap-[10px]">
                   <span className="text-[25px] font-semibold">Login Page</span>
-                  <span className="text-[16px]">Welcome to OneCart, Apply to Admin Login</span>
+                  <span className="text-[16px]">Welcome to EchoCart, Apply to Admin Login</span>
                 </div>
                 <div className="max-w-[600px] w-[90%] h-[400px] bg-[#00000025] border-[1px] border-[#96969635] backdrop:blur-2xl rounded-lg shadow-lg flex items-center justify-center">
                        <form onSubmit={AdminLogin} className=" w-[90%] h-[90%] flex flex-col items-center " action="">
